d6c2b5283f11efc5c3359d728f1f87446f793798
neptali <neptali@plerk.io>
2023-03-28 20:01:34 -0400

    download file

--- main.py
+++ main.py
@@ -1,56 +1,28 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.wait import WebDriverWait
-import urllib.request
-
 dp="chromedriver"
 chrome_options= Options()
-
 # initialize the Chrome webdriver
 chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(dp, options= chrome_options)
-
 # go to Moddb website
 driver.get("https://www.moddb.com/")
-
 # find the search bar and enter "Dark-Life 2"
-search_bar = driver.find_element(By.ID,'sitesearch')  #Corregido Chuma , era "sitesearch"
+search_bar = driver.find_element(By.ID,'sitesearch')  #"sitesearch"
 search_bar.send_keys("Dark-Life 2")
 search_bar.submit()
+darklife_link2 = driver.find_element(By.XPATH,"//a[contains(normalize-space(), 'Dark Life 2 demo')]")
+darklife_link2.click()
+#get current window handle
+p = driver.current_window_handle
+#get first child window
+chwd = driver.window_handles
+for w in chwd:
+    if(w!=p):
+        driver.switch_to.window(w)
+get_url = driver.current_url
+print("The current url is:"+str(get_url))
+darklife_link3 = driver.find_element(By.XPATH,"//a[contains(normalize-space(), 'Download now')]")
+darklife_link3.click()
 
-
-
-darklife_link = driver.find_element(By.XPATH,"//a[text(),'dark-life 2 mod for']") #En esta parte el script deberia acceder a la pagina, no lo hace, y para mi el XPath parcial esta bien, ya lo hice con contain y sin contain y nada , check pls ily
-darklife_link.click()
-
-# go to the Files section
-files_link = driver.find_element(By.XPATH,"//a[contains(@href,'files')]")
-files_link.click()
-
-# download the demo file
-demo_link = driver.find_element(By.XPATH,"//a[contains(@href,'dark-life-2-demo')]")
-demo_url = demo_link.get_attribute("href")
-urllib.request.urlretrieve(demo_url,r"C:\Users\luism\Downloads\DarkLife2Demo.zip")
-# Template download link
-#urllib.request.urlretrieve(demo_url,"DarkLife2Demo.zip")
-
-
-# close the webdriver
-driver.quit()
-#Note that you may need to modify the file path for the downloaded file ("DarkLife2Demo.zip") to save it to the desired location on your system. Also, make sure to have the urllib module imported for downloading the file.
-
-
-#C:\Users\luism\Downloads is the download path
-#Great! In that case, you can modify the urllib.request.urlretrieve line to the following:
-
-#python
-#Copy code
-#urllib.request.urlretrieve(demo_url, r"C:\Users\luism\Downloads\DarkLife2Demo.zip")
-#This will save the downloaded file to the specified path on your system.
-
-
-# go to the Dark-Life 2 page
-# Indev Chuma estas lineas
-#WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "select#suggest_search"))).click()
-#select = Select(WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "select#suggest_search"))))
