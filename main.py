c3589165abb021c45cadaa6240d68bc08cea9e61
neptali <neptali@plerk.io>
2023-03-28 20:05:36 -0400

    formatting

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-dp="chromedriver"
-chrome_options= Options()
+dp = "chromedriver"
+chrome_options = Options()
 # initialize the Chrome webdriver
 chrome_options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(dp, options= chrome_options)
+driver = webdriver.Chrome(dp, options=chrome_options)
 # go to Moddb website
 driver.get("https://www.moddb.com/")
 # find the search bar and enter "Dark-Life 2"
-search_bar = driver.find_element(By.ID,'sitesearch')  #"sitesearch"
+search_bar = driver.find_element(By.ID, 'sitesearch')  # "sitesearch"
 search_bar.send_keys("Dark-Life 2")
 search_bar.submit()
-darklife_link2 = driver.find_element(By.XPATH,"//a[contains(normalize-space(), 'Dark Life 2 demo')]")
+darklife_link2 = driver.find_element(
+    By.XPATH, "//a[contains(normalize-space(), 'Dark Life 2 demo')]")
 darklife_link2.click()
-#get current window handle
+# get current window handle
 p = driver.current_window_handle
-#get first child window
+# get first child window
 chwd = driver.window_handles
 for w in chwd:
-    if(w!=p):
+    if w != p:
         driver.switch_to.window(w)
 get_url = driver.current_url
 print("The current url is:"+str(get_url))
-darklife_link3 = driver.find_element(By.XPATH,"//a[contains(normalize-space(), 'Download now')]")
+darklife_link3 = driver.find_element(
+    By.XPATH, "//a[contains(normalize-space(), 'Download now')]")
 darklife_link3.click()
-
